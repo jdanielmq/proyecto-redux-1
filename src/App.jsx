7f22c9087673ee7439b9058a9996b850333b55ef
Juan Daniel Muñoz Queupul <munoz.queupul.daniel.juan@gmail.com>
2020-06-30 11:30:37 -0400

    limipieza de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="text-center">Proyecto con Redux </h1>
+    </div>
+  );
+}
+
+export default App;
